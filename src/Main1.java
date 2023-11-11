50dcffcd87966750e4697e98fbd94972e81ac313
HDiyar <hdiyar998@gmail.com>
2023-11-12 00:21:57 +0100

    unnötige klassen wurden entfernt

--- src/Main1.java
+++ src/Main1.java
@@ -7,40 +7,39 @@ import java.sql.SQLException;
  *
  * @author Diyar
  */
-public class Main1 extends DBZugriff {
-
-    public static void main(String[] args) throws SQLException {
-        connect();
-
-        String sql = "SELECT benutzerId, vorname, nachname FROM T_Benutzer";
-
-        ResultSet rs = befehl.executeQuery(sql);
-        while (rs.next())
-        {
-            String bId = rs.getString(1);
-            String vorname = rs.getString(2).trim();
-            String nachname = rs.getString(3).trim();
-
-            System.out.println("benutzerId: " + bId + " Nachname: " + nachname + " Vorname: " + vorname);
-        }
-        close();
-
-    }
-}
-
-//import datenhaltungsschicht.DBZugriff;
-//import java.sql.Connection;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import praesentationsschicht.KonsolenMenue;
-//
-///**
-// *
-// * @author Diyar
-// */
 //public class Main1 extends DBZugriff {
 //
 //    public static void main(String[] args) throws SQLException {
-//        new KonsolenMenue().startMenu();
+//        connect();
+//
+//        String sql = "SELECT benutzerId, vorname, nachname FROM T_Benutzer";
+//
+//        ResultSet rs = befehl.executeQuery(sql);
+//        while (rs.next())
+//        {
+//            String bId = rs.getString(1);
+//            String vorname = rs.getString(2).trim();
+//            String nachname = rs.getString(3).trim();
+//
+//            System.out.println("benutzerId: " + bId + " Nachname: " + nachname + " Vorname: " + vorname);
+//        }
+//        close();
+//
 //    }
 //}
+import datenhaltungsschicht.DBZugriff;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import praesentationsschicht.KonsolenMenue;
+
+/**
+ *
+ * @author Diyar
+ */
+public class Main1 extends DBZugriff {
+
+    public static void main(String[] args) throws SQLException {
+        new KonsolenMenue().startMenu();
+    }
+}
